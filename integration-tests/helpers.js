e52fdb462770a61169f07d7c08c9bb1a58674acb
samreen2019 <56146830+samreen2019@users.noreply.github.com>
2021-02-22 16:37:33 +0500

    first commit

---
+++ integration-tests/helpers.js
@@ -0,0 +1,10 @@
+import webdriver from 'selenium-webdriver';
+import chrome from 'chromedriver';
+
+
+export const getDriver = () => {
+  let driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+  return driver;
+};
