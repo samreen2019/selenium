f91c3da97201321edbfaa46245fb22f00b0cbfbd
samreen2019 <56146830+samreen2019@users.noreply.github.com>
2021-06-07 02:28:46 +0500

    Update helpers.js
    driver changed

--- integration-tests/helpers.js
+++ integration-tests/helpers.js
@@ -1,10 +1,23 @@
 import webdriver from 'selenium-webdriver';
 import chrome from 'chromedriver';
-
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument("--no-sandbox") # linux only
 
 export const getDriver = () => {
   let driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
+  driver = webdriver.Chrome(options=chrome_options)
   return driver;
 };
+
+
+
+
+
+
+
+
+
+
