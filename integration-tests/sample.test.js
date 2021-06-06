8ac3b3540a0b6543c3fb7a8f46778ca8aa482cf6
samreen2019 <56146830+samreen2019@users.noreply.github.com>
2021-06-07 03:28:07 +0500

    Update sample.test.js

--- integration-tests/sample.test.js
+++ integration-tests/sample.test.js
@@ -1,15 +1,11 @@
+import {getDriver} from './helpers';
 import {until} from 'selenium-webdriver';
 
-import webdriver from 'selenium-webdriver';
-import chrome from 'chromedriver';
+let driver;
 
-const chromeCapabilities = webdriver.Capabilities.chrome();
-chromeCapabilities.set('chromeOptions', {args: ['--no-sandbox']});
-
-let driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .withCapabilities(chromeCapabilities)
-    .build(); 
+beforeAll(() => {
+  driver = getDriver();
+});
 
 afterAll(async () => {
   await driver.quit();
@@ -19,6 +15,3 @@ test('should have a title', async  () => {
   await driver.get('http://localhost:3000');
   await driver.wait(until.titleIs('React App'), 1000);
 });
-
-
-
