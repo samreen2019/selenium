b94f22f1fd9d17e70ab80b04e616704ea71ce85f
samreen2019 <56146830+samreen2019@users.noreply.github.com>
2021-06-10 15:36:39 +0500

    Update helpers.js

--- integration-tests/helpers.js
+++ integration-tests/helpers.js
@@ -1,18 +1,14 @@
 import webdriver from 'selenium-webdriver';
-import chrome from 'chromedriver';
-
-const chromeCapabilities = webdriver.Capabilities.chrome();
-chromeCapabilities.set('chromeOptions', {args: ['--no-sandbox']});
-
-
+import chromewebdriver  from "selenium-webdriver/chrome"
+const chromeOptions = new chromewebdriver.Options();
+chromeOptions.addArguments("--no-sandbox");
 export const getDriver = () => {
   let driver = new webdriver.Builder()
     .forBrowser('chrome')
-    .withCapabilities(chromeCapabilities)
+    .setChromeOptions(chromeOptions)
     .build();
   return driver;
 };
-
 
 
 
